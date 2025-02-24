2358ec5f28490168d819abfc67e89f94cebdeaad
NH <mnh4140@gmail.com>
2025-02-24 17:14:38 +0900

    앱 기능 구현 1. 랜덤 색상 생성: 버튼 클릭 시 배경색이 랜덤하게 변경 2. 색상 정보 표시:현재 화면 배경색의 RGB 값을 화면에 표시 3. 초기화 기능: 초기화 버튼을 누르면 배경색이 흰색으로 변경되고, RGB 값도 초기화

--- iOS_App_Random_Color/ViewController.swift
+++ iOS_App_Random_Color/ViewController.swift
@@ -9,13 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myView: UIView!
     @IBOutlet weak var myLabel: UILabel!
-    
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    // Change Button 클릭 Action
+    @IBAction func didTapChangeButton(_ sender: Any) {
+        // 랜덤 RGB 값을 받기
+        let randomRed = CGFloat.random(in: 0...1)
+        let randomGreen = CGFloat.random(in: 0...1)
+        let randomBlue = CGFloat.random(in: 0...1)
+        let randomColor = UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+        
+        // 배경색 랜덤 RGB 색으로 설정
+        myView.backgroundColor = randomColor
+        
+        // RGB 출력을 위해 CGFloat 값에 255 곱한 값 저장
+        // CGFloat은 0 부터 1 사이의 부동소수 값을 가지고 색을 표현하므로, 255를 곱하지 않으면 소수으로 출력
+        let red = Int(randomRed * 255)
+        let green = Int(randomGreen * 255)
+        let blue = Int(randomBlue * 255)
+        
+        // 배경색 RGB 값 출력
+        myLabel.text = "R: " + String(red) + ", G: " + String(green) + ", B: " + String(blue)
+    }
+    
+    // Reset Buton 클릭 Action
+    @IBAction func didTapResetButon(_ sender: Any) {
+        // 배경색 흰색으로 변경을 위해 값 저장 변수
+        let resetColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        // 배경색 흰색으로 변경
+        myView.backgroundColor = resetColor
+        
+        // 배경색 RGB 값 출력
+        myLabel.text = "R: 255, G: 255, B: 255"
+    }
 }
-
