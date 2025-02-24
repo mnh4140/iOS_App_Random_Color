4d1dc06477b0886aea6db8a55788cb924e73c66f
NH <mnh4140@gmail.com>
2025-02-24 15:15:45 +0900

    Initial Commit

---
+++ iOS_App_Random_Color/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_App_Random_Color
+//
+//  Created by NH on 2/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS_App_Random_ColorTests/iOS_App_Random_ColorTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_App_Random_ColorTests.swift
+//  iOS_App_Random_ColorTests
+//
+//  Created by NH on 2/24/25.
+//
+
+import Testing
+@testable import iOS_App_Random_Color
+
+struct iOS_App_Random_ColorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
